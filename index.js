318409c30ccb86271b8044be7ecf62eff84b6d36
Akansha Verma <120491297+akansha-1202@users.noreply.github.com>
2023-07-05 20:26:59 +0530

    chnge

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = http.createServer((req,res) =>{
     //http://localhost:4000/json
     else if(req.url=='/json'){
         const node ={
-            "<h1>What is Node.JS?</h1>": "<p>Node.js is an open-source, cross-platform runtime environment that allows you to run JavaScript code on the server-side. It uses the V8 JavaScript engine, developed by Google, which is known for its fast and efficient execution of JavaScript code. Node.js is built on an event-driven, non-blocking I/O model, making it particularly suitable for building scalable and high-performance network applications.</p>"
+            "What is Node.JS?": "Node.js is an open-source, cross-platform runtime environment that allows you to run JavaScript code on the server-side. It uses the V8 JavaScript engine, developed by Google, which is known for its fast and efficient execution of JavaScript code. Node.js is built on an event-driven, non-blocking I/O model, making it particularly suitable for building scalable and high-performance network applications."
         }
         res.write(JSON.stringify(node));
         res.end()
